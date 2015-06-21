1e877dba3f4ac83bb4c3e4cfc70869012f906d66
은현 <nyu531@naver.com>
2015-06-21 21:22:53 +0900

    글올리기, 새로고침 버튼 등등

--- telepathy/src/edu/skku/sosil3/sky/telepathy/TabActivity.java
+++ telepathy/src/edu/skku/sosil3/sky/telepathy/TabActivity.java
@@ -1,14 +1,28 @@
 package edu.skku.sosil3.sky.telepathy;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -16,14 +30,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,12 +45,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
-import android.widget.TabWidget;
-import android.widget.Toast;
 import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabWidget;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class TabActivity extends Activity implements OnRefreshListener {
+public class TabActivity extends Activity {
 
 	TabHost tabHost;
 	ListView ListOne;
@@ -55,12 +68,17 @@ public class TabActivity extends Activity implements OnRefreshListener {
 	ActionBar actionBar;
 	String id;
 	Intent intent;
-	SwipeRefreshLayout refreshLayout;
 	
 	static String SAMPLEIMG = "photo.png";
 	private final static int ACT_CAMERA = 1;
 	private final static int ACT_GALLERY = 2;
 	private final static int SETTINGS_INFO = 3;
+	
+	static int upload_state = 0;
+	static final int DEFAULT = 0;
+	static final int SUCCESS = 1;
+	static final int LOCATION_NOT_FOUND = 2;
+	static final int NETWORK_ERROR = 3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +101,6 @@ public class TabActivity extends Activity implements OnRefreshListener {
 		date = (TextView) findViewById(R.id.one_date);
 		image = (ImageView) findViewById(R.id.one_image);
 		content = (EditText) findViewById(R.id.one_content);
-		refreshLayout = (SwipeRefreshLayout) findViewById(R.id.two_swipe);
 		
 		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
 		date.setText(format.format(new Date()));
@@ -91,8 +108,6 @@ public class TabActivity extends Activity implements OnRefreshListener {
 		actionBar = getActionBar();
 		actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#7beda7")));
 		actionBar.setIcon(R.drawable.white);
-		
-		// refreshLayout.setOnRefreshListener(this); XXX
 		
 		// 서버 연결, 데이터 수집
 		ArrayList<PostItem> array1 = new ArrayList<PostItem>();
@@ -162,7 +177,7 @@ public class TabActivity extends Activity implements OnRefreshListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.tab, menu);
+		getMenuInflater().inflate(R.menu.custom_tab, menu);
 		return true;
 	}
 
@@ -175,6 +190,9 @@ public class TabActivity extends Activity implements OnRefreshListener {
 		if (id == R.id.action_settings) {
 			startActivityForResult(new Intent(this, SettingsActivity.class), SETTINGS_INFO);
 			return true;
+		}
+		else if(id == R.id.action_refresh){
+			// 새로고침
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -254,32 +272,122 @@ public class TabActivity extends Activity implements OnRefreshListener {
 	}
 	
 	public void onFinish(View v){
-		Boolean res_anonymity = anonymity.isChecked();
-		String res_user;
-		String res_date = date.getText().toString();
-		String res_content = content.getText().toString();
-		
-		if(res_anonymity){
-			res_user = "익명";
-		}
-		else{
-			res_user = null;
-		}
-		
-		// 서버에 글을 등록
-	}
+	      Boolean res_anonymity = anonymity.isChecked(); // 익명 여부
+	      
+	      final String res_user; // 이름
+	      final String res_content = content.getText().toString(); // 글
+	      final String res_image = ""; // 이미지
+	      
+	      LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+	      GPSLocationListener gpsLocationListener = new GPSLocationListener(locationManager);
+	      final double res_latitude = gpsLocationListener.getLatitude(); // 위도
+	      final double res_longitude = gpsLocationListener.getLongitude(); // 경도
+	      gpsLocationListener.stopGettingLocation();
+	      
+	      if(res_anonymity) { // 익명 여부 판단
+	         res_user = "익명";
+	      } else {
+	         res_user = id;
+	      }
+	      
+	      // 서버에 글을 등록
+	      new Thread() {
+	            
+	            @SuppressWarnings("deprecation")
+	            public void run() {
+	               upload_state = DEFAULT;
 
-	@Override
-	public void onRefresh() {
-		// TODO Auto-generated method stub
-		new Handler().postDelayed(new Runnable(){
+	            String uriString = "http://telepathy.hyunha.org/post"; // 업로드할 url
+	            ArrayList<BasicNameValuePair> Parameter = new ArrayList<BasicNameValuePair>();
 
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				refreshLayout.setRefreshing(false);
-			}
-			
-		}, 2000);
-	}
+	            try {
+	               URI uri = new URI(uriString);
+	               HttpClient httpclient = new DefaultHttpClient();
+	               HttpPost httpPost = new HttpPost(uri);
+
+	               Parameter.add(new BasicNameValuePair("p_user", res_user));
+	               Parameter.add(new BasicNameValuePair("p_image", res_image));
+	               Parameter.add(new BasicNameValuePair("p_content", res_content));
+	               Parameter.add(new BasicNameValuePair("p_lati", Double.toString(res_latitude)));
+	               Parameter.add(new BasicNameValuePair("p_long", Double.toString(res_longitude)));
+	               
+	               Log.d("Upload", "Uploading data");
+	               Log.d("Upload", "- "+res_user);
+	               Log.d("Upload", "- "+res_image);
+	               Log.d("Upload", "- "+res_content);
+	               Log.d("Upload", "- "+Double.toString(res_latitude));
+	               Log.d("Upload", "- "+Double.toString(res_longitude));
+	               
+	               httpPost.setEntity(new UrlEncodedFormEntity(Parameter,"UTF-8"));
+
+	               // JSON data 얻어내기
+	               HttpResponse httpResponse = httpclient.execute(httpPost);
+	               String responseString = EntityUtils.toString(
+	                     httpResponse.getEntity(), HTTP.UTF_8);
+	               Log.d("Upload", "responseString");
+	               Log.d("Upload", responseString);
+
+	               // JSON data에서 success 여부 알아내기
+	               JSONObject jsonObject = new JSONObject(responseString);
+	               String success = jsonObject.getString("success");
+	               Log.d("Upload", "success : " + success);
+
+	               if (res_latitude != GPSLocationListener.DATA_GET_FAILED) upload_state = SUCCESS;
+	               else upload_state = LOCATION_NOT_FOUND;
+
+	            } catch (IOException e) {
+	               upload_state = NETWORK_ERROR;
+	            } catch (JSONException e) {
+	               upload_state = NETWORK_ERROR;
+	            } catch (Exception e) {
+	               e.printStackTrace();
+	            }
+
+	            if (upload_state == SUCCESS) { // 업로드 성공
+	               Log.d("Upload", "comment : SUCCESS");
+	               runOnUiThread(new Runnable() {
+	                  public void run() {
+	                     Toast.makeText(getApplicationContext(),
+	                           "성공적으로 업로드하였습니다.", Toast.LENGTH_SHORT)
+	                           .show();
+	                  }
+	               });
+	               startActivity(new Intent(getApplicationContext(), TabActivity.class));
+	               finish();
+	               
+	            } else if (upload_state == LOCATION_NOT_FOUND) { // 업로드 성공 - 좌표 찾기 실패
+	               Log.d("Upload", "comment : LOCATION_NOT_FOUND");
+	               runOnUiThread(new Runnable() {
+	                  public void run() {
+	                     Toast.makeText(getApplicationContext(),
+	                           "좌표 찾기에 실패하였습니다. 좌표에 대한 정보 없이 업로드합니다.", Toast.LENGTH_SHORT)
+	                           .show();
+	                  }
+	               });
+	               startActivity(new Intent(getApplicationContext(), TabActivity.class));
+	               finish();
+
+	            } else if (upload_state == NETWORK_ERROR) { // 업로드 실패 - 네트워크 오류
+	               Log.d("Upload", "comment : NETWORK_ERROR");
+	               runOnUiThread(new Runnable() {
+	                  public void run() {
+	                     Toast.makeText(getApplicationContext(),
+	                           "네트워크 상태를 확인하십시오.", Toast.LENGTH_SHORT)
+	                           .show();
+	                  }
+	               });
+
+	            } else { // 업로드 실패 - 알 수 없는 오류
+	               Log.d("Register", "comment : DEFAULT");
+	               runOnUiThread(new Runnable() {
+	                  public void run() {
+	                     Toast.makeText(getApplicationContext(),
+	                           "알 수 없는 오류가 발생했습니다.", Toast.LENGTH_SHORT)
+	                           .show();
+	                  }
+	               });
+	            }
+	         }
+	      }.start(); // thread end
+	   }
 }
