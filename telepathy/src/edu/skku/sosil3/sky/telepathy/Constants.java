15ec9ccbbf6fb17f3266bd707689024272fc8d09
HyunHa Park <park@hyunha.org>
2015-06-21 20:38:14 +0900

    modify

--- telepathy/src/edu/skku/sosil3/sky/telepathy/Constants.java
+++ telepathy/src/edu/skku/sosil3/sky/telepathy/Constants.java
@@ -1,18 +1,18 @@
 package edu.skku.sosil3.sky.telepathy;
 
 public class Constants {
-	static final long BUILD_NO = 0;
+	public static final long BUILD_NO = 0;
 	
-	static final String URL_SERVER_HOST = "http://telepathy.hyunha.org";
+	public static final String URL_SERVER_HOST = "http://telepathy.hyunha.org";
 	
 	/* Main SNS functionalities. */
-	static final String URI_POST_SIGNUP = "/signup";
-	static final String URI_POST_SIGNIN = "/signin";
-	static final String URI_POST_POST = "/post";
-	static final String URI_POST_COMMENT = "/comment";
-	static final String URI_GET_NEWSFEED = "/newsfeed";
+	public static final String URI_POST_SIGNUP = "/signup";
+	public static final String URI_POST_SIGNIN = "/signin";
+	public static final String URI_POST_POST = "/post";
+	public static final String URI_POST_COMMENT = "/comment";
+	public static final String URI_GET_NEWSFEED = "/newsfeed";
 	
 	/* Update Checking. */
-	static final String URI_GET_MIN_VERSION = "/app/android/min-version";
-	static final String URI_GET_LATEST_VERSION = "/app/android/latest-version";
+	public static final String URI_GET_MIN_VERSION = "/app/android/min-version";
+	public static final String URI_GET_LATEST_VERSION = "/app/android/latest-version";
 }
